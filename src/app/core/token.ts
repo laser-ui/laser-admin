5d48eff0a372536caff7acf2b335bb10264db465
xiejay <xiejay97@gmail.com>
2024-06-11 15:57:58 +0800

    chore: update deps

--- src/app/core/token.ts
+++ src/app/core/token.ts
@@ -1,13 +1,23 @@
 import { JWTToken } from '@laser-pro/auth';
 import { axios } from '@laser-pro/http';
 
-export const TOKEN = new JWTToken({});
-export const rememberToken = (remember: boolean) => {
-  TOKEN.refresh = remember
-    ? () =>
+let remember = false;
+export const TOKEN = new JWTToken({
+  refresh: () =>
+    new Promise((r) => {
+      if (remember) {
         axios({
           url: '/auth/refresh',
           method: 'post',
-        }).then((res) => res.data)
-    : false;
+        }).then((res) => {
+          r(res.data);
+        });
+      } else {
+        r(null);
+      }
+    }),
+});
+
+export const rememberToken = (val: boolean) => {
+  remember = val;
 };
